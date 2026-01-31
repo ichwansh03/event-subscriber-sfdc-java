12e36d9ba12f246af146096eb52e33d26aaee18f
ichwansh03 <ichwansholihin70@gmail.com>
2026-01-31 11:10:20 +0700

    initial project

---
+++ src/main/java/com/ichwan/sfdcevent/SfdceventApplication.java
@@ -0,0 +1,13 @@
+package com.ichwan.sfdcevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfdceventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfdceventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ichwan/sfdcevent/SfdceventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ichwan.sfdcevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfdceventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
